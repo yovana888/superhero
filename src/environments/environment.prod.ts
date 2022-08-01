1f1f5ed61d71505eb40fc6468e51afdc1ef27631
yovana888 <yovana.otaku@gmail.com>
2022-08-01 16:57:03 -0500

    env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCNBCgkTluKOtzsAlAkbbZbjIM2ynwIa-M",
+    authDomain: "superhero-f3ae7.firebaseapp.com",
+    projectId: "superhero-f3ae7",
+    storageBucket: "superhero-f3ae7.appspot.com",
+    messagingSenderId: "127842759091",
+    appId: "1:127842759091:web:5862a9a6fa28b2f87cdbc6"
+  },
+  apiUrl: 'https://gateway.marvel.com:443/v1/public/',
+  apiKey: 'eedc48d0898559783fc9d35e6e6b72bf',
 };
